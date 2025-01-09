04dd7c95daa60540331bde1cd868027af3fefa9b
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-10 06:34:09 +0700

    Remove generated API documentation and related files from the target directory

--- target/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.itsme","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"com.itsme","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
